1816043252bd6533c9a8e11a7953dbf0469e0e74
Devi Acharya <dacharya64@gmail.com>
2020-02-03 12:36:18 -0800

    Adding json of locations

---
+++ sim/data/locations.js
@@ -0,0 +1,82 @@
+var json_locations = `{
+  "1": {
+    "name": "The Stonehill Inn", 
+    "state": "undiscovered",
+    "NPCs": [Toblen Stonehill, Narth, Elsa, Lanar, Trilena, Pip, Freda]
+  },
+  "2": {
+    "name": "Lionshield Coster", 
+    "state": "undiscovered",
+    "NPCs": [Linene Graywind]
+  },
+  "3": {
+    "name": "Barthen's Provisions", 
+    "state": "undiscovered",
+    "NPCs": [Elmar Barthen, Ander, Thistle]
+  },
+  "4": {
+    "name": "Townmaster's Hall", 
+    "state": "undiscovered",
+    "NPCs": [Harbin Wester]
+  },
+  "5": {
+    "name": "Shrine of Luck", 
+    "state": "undiscovered",
+    "NPCs": [Sister Garaele]
+  },
+  "6": {
+    "name": "Sleeping Giant tap house", 
+    "state": "undiscovered",
+    "NPCs": [Grista, Redbrand thugs]
+  },
+  "7": {
+    "name": "Edermath Orchard", 
+    "state": "undiscovered",
+    "NPCs": [Daran Edermath]
+  },
+  "8": {
+    "name": "Tresendar Manor", 
+    "state": "undiscovered",
+    "NPCs": []
+  },
+  "9": {
+    "name": "Old Owl Well", 
+    "state": "undiscovered",
+    "NPCs": []
+  },
+  "10": {
+    "name": "Phandalin Miner’s Exchange", 
+    "state": "undiscovered",
+    "NPCs": [Halia Thornton]
+  },
+  "11": {
+    "name": "Alderleaf Farm", 
+    "state": "undiscovered",
+    "NPCs": [Qelline Alderleaf, Carp],
+  },
+  "12": {
+    "name": "Thundertree", 
+    "state": "undiscovered",
+    "NPCs": []
+  },
+  "13": {
+    "name": "Agatha's Lair", 
+    "state": "undiscovered",
+    "NPCs": []
+  },
+  "14": {
+    "name": "Triboar Trail", 
+    "state": "undiscovered",
+    "NPCs": []
+  },
+  "15": {
+    "name": "Cragmaw Castle", 
+    "state": "undiscovered",
+    "NPCs": []
+  },
+  "16": {
+    "name": "Wave Echo Cave", 
+    "state": "undiscovered",
+    "NPCs": []
+  } 
+}`
